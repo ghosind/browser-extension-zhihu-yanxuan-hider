95e81ea46df29d4430a8b82217a2d032406c3c9a
Chen Su <ghosind@gmail.com>
2024-12-09 11:53:36 +0800

    feat: set yanxuan content card's display style to none
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- shared/scripts/yanxuan.js
+++ shared/scripts/yanxuan.js
@@ -1,31 +1,9 @@
+const ContainerClassName = 'ContentItem';
 const YanXuanClassName = 'KfeCollection-AnswerTopCard-Container';
 
 const hideYanXuanAnswers = () => {
-  const yanXuanAnswers = document.getElementsByClassName(YanXuanClassName);
-
-  if (!yanXuanAnswers || yanXuanAnswers.length === 0) {
-    return;
-  }
-
-  for (let answer of yanXuanAnswers) {
-    let list;
-    let parent = answer.parentElement;
-
-    while (parent) {
-      if (/(List-item|TopstoryItem)/.test(parent.className)) {
-        list = parent;
-        break;
-      }
-
-      parent = parent.parentElement;
-    }
-
-    if (!list) {
-      continue;
-    }
-
-    list.style.display = 'none';
-  }
+  document.styleSheets[0]
+    .insertRule(`.${ContainerClassName}:has(.${YanXuanClassName}) { display: none !important; }`);
 }
 
 hideYanXuanAnswers();
